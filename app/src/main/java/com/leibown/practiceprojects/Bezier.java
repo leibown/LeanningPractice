4082468fdc104a3a5fc23f35c0f34ca0eaa0c249
Leibown <leibown.leila@gmail.com>
2016-11-22 17:46:07 +0800

    贝塞尔曲线

--- app/src/main/java/com/leibown/practiceprojects/Bezier.java
+++ app/src/main/java/com/leibown/practiceprojects/Bezier.java
@@ -1,5 +1,9 @@
 package com.leibown.practiceprojects;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * 贝塞尔曲线学习
  *
@@ -7,6 +11,14 @@ package com.leibown.practiceprojects;
  *         created at 2016/11/22 17:29
  */
 
-public class Bezier {
+public class Bezier extends View {
 
+    public Bezier(Context context) {
+        this(context, null);
+    }
+
+    public Bezier(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
 }
